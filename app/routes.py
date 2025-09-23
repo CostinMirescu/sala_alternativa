7ded051965fbd98aadc9da71a1d97f2ae758b74f
CostinMirescu <147921841+CostinMirescu@users.noreply.github.com>
2025-09-23 06:55:50 +0300

    Update routes.py

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+# app/routes.py
+from flask import Blueprint, render_template
+
+
+bp = Blueprint("main", __name__)
+
+
+@bp.get("/")
+def home():
+return render_template("index.html", title="Sala Alternativă")
